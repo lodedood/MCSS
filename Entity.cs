4cd5fc04db35a290df2422aa272999bac62852ff
lodedood <52197266+lodedood@users.noreply.github.com>
2019-10-07 13:36:06 +0700

    Add files via upload

---
+++ Entity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCSS.Entities
+{
+    public abstract class Entity
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+    }
+    
+    public abstract class EntityTimeStamp : Entity
+    {
+        public DateTime Created { get; set; }
+
+        [StringLength(255)]
+        public string CreatedBy { get; set; }
+
+        public DateTime Updated { get; set; }
+
+        [StringLength(255)]
+        public string UpdatedBy { get; set; }
+
+    }
+
+}
